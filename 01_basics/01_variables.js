fdef9c5e7ac3fa00a3c8cf471005fdaacb55daf9
shaiksafoora07-ship-it <shaiksafoora07@gmail.com>
2026-02-12 15:22:44 +0000

    add variables 01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1278
+let accountEmail = "safu@gmail.com"
+var accountPassword = "12345"
+accountCity = "bengaluru"
+
+accountEmail = "saf@gmail.com"
+accountPassword = "09876"
+accountCity = "jaipur"
+let accountState;
+
+
+// accountId = 2;  NOT ALLOWED
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
