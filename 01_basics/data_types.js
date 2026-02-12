ad1e7afa52cb5a16ee6231ec443c5ad0b74e409f
shaiksafoora07-ship-it <shaiksafoora07@gmail.com>
2026-02-12 17:09:32 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,28 @@
+"use strict";   // treat all js code as newer version
+//alert(3+3)    //we are using nodeJs,not browser--gives error here
+console.log(3
+    +3)// readability is not good // readability should be high
+//console.log("safoora")
+
+let name = "safoora"
+let age = 20
+let isLoggedIn = false;
+
+//DATA TYPES
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value (not only type)
+//undefined => (value not assigned)
+//symbol => unique
+
+
+//PRIMITIVE DATA TYPES
+//object
+console.log(typeof name)        //string
+console.log(typeof age)         //number
+console.log(typeof number)      //undefined
+//interview question
+console.log(typeof undefined)   //undefined
+console.log(typeof null)        //object
